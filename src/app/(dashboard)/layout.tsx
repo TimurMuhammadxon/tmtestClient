58b2b5f31c863c93ffddf9d09d21837b75ac4509
TimurMuhammadxon <timurmuhammadxon@gmail.com>
2025-09-21 05:36:57 +0500

    update20250921

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href ="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image src = {logo} alt ="logo" width={32} height={32}/>
-          <span className="hidden lg:block">TMtest</span>
+          <span className="hidden lg:block">TM test</span>
         </Link>
         <Menu/>
       </div>
